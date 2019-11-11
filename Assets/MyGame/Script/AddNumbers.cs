56abf79e8f7c2c6b78478147a76125908557d859
unknown <mira.trampitsch02@htl-salzburg.ac.at>
2019-11-11 19:47:38 +0100

    add changes to script

--- Assets/MyGame/Script/AddNumbers.cs
+++ Assets/MyGame/Script/AddNumbers.cs
@@ -7,7 +7,22 @@ public class AddNumbers : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int a = 3;
+
+        int b = 4;
+
+
+
+        Debug.Log("a = " + a);
+
+        Debug.Log("b = " + b);
+
+        Debug.Log("a + b = " + (a + b));
+
+
+
+        Debug.Log("add numbers a = " + a + ", b = " + b + "; a + b = " + (a + b));
+
     }
 
     // Update is called once per frame
